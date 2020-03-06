2848c06bfdaf9fada0c5f5a2f7b07fa5782eaf50
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-03-06 12:27:30 -0300

    Fibonacci Upload

---
+++ Fibonacci.py
@@ -0,0 +1,29 @@
+# Begins here
+def fibonacci(i):
+    a = 1
+    b = 1
+    c = 2
+
+    print(a)
+    print(b)
+    print(c)
+
+    i -= 3
+
+    while i > 0:
+        a = b + c
+        b = c
+        c = a
+
+        i -= 1
+
+        print(c)
+
+
+# Ends here
+i = int(input("Type the index of Fibonacci Sequence desired: "))
+
+# Just a test above
+fibonacci(i)
+
+print("End of program :D")
