d2ea971a838461be8070aaf5f4c4ca44627e19a7
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-06-24 19:37:58 -0300

    Now create a file with the index desired

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,7 @@
+import time
+
+f = open("FibonacciOutput.txt", "w+")
+
 # Begins here
 def fibonacci(i):
     a = 1
@@ -18,12 +22,20 @@ def fibonacci(i):
         i -= 1
 
         print(c)
+    
+    f.write(str(c) + "\n")
 
 
 # Ends here
 i = int(input("Type the index of Fibonacci Sequence desired: "))
 
+inicial = int(time.strftime("%S", time.localtime())) + 60 * int(time.strftime("%M", time.localtime())) + 3600 * int(time.strftime("%H", time.localtime()))
+
 # Just a test below
 fibonacci(i)
 
 print("End of program :D")
+
+final = int(time.strftime("%S", time.localtime())) + 60 * int(time.strftime("%M", time.localtime())) + 3600 * int(time.strftime("%H", time.localtime())) - inicial
+
+print("Elapsed time: " + str(final) + " second(s)")
